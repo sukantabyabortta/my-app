59a3add32d82e2531a71e821ba9a8ab96397fd94
SukantaWeb <sukanta.byabortta@gmail.com>
2026-03-19 12:43:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css'
 import App from './App.jsx';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // import reportWebVitals  from "./reportWebVitals.js";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+  <React.StrictMode>
+  <HashRouter>
     <App />
-  </BrowserRouter>
-  </StrictMode>,
+  </HashRouter>
+</React.StrictMode>,
 );
 
 // reportWebVitals();
