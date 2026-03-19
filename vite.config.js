e87b0a0d58c70e6619bf93904b19f8aef067f202
SukantaWeb <sukanta.byabortta@gmail.com>
2026-03-19 12:27:50 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/my-app/', // 👈 VERY IMPORTANT (your repo name)
   plugins: [
     react({
       babel: {
